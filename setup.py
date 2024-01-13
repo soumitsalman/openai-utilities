d3931d783c954fa2fc1c3ea6999e26310ebabb60
soumitsalman <soumitsr@gmail.com>
2024-01-13 14:10:53 -0500

    updating version number to make pypi happy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='openai-utilities',
-    version='0.1.0',
+    version='0.2.0',
     description='Wrapper on top of openai python SDK primarily to interface with openai and anyscale public endpoints.',
     url='https://github.com/soumitsalman/openai-utilities',
     author='Soumit Salman Rahman',
